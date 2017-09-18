7dc6e108199f3bfe9a3853c7a52edd70c87a045e
RM004134263 <RM004134263@MNL02S2021204D1>
2017-09-18 09:47:57 -0500

    change made in employee info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
@@ -28,7 +28,6 @@ import { AdminEmployeeInfoComponent } from './admin/admin-employee-info/admin-em
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule,
     NgbModule.forRoot(),
     RouterModule.forRoot([
       { path: '', component: HomeComponent },

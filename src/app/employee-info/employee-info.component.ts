b10f3544c32fa647f7aea9e402bdb0bc144888d1
RM004134263 <RM004134263@MNL02S2021204D1>
2017-09-19 08:11:35 -0500

    commit print

--- src/app/employee-info/employee-info.component.ts
+++ src/app/employee-info/employee-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularPrint } from 'angular-print';
 
 @Component({
   selector: 'app-employee-info',
@@ -8,6 +9,32 @@ import { Component, OnInit } from '@angular/core';
 export class EmployeeInfoComponent implements OnInit {
 
   constructor() { }
+
+      print(printSectionId): void {
+        let printContents, popupWin;
+        printContents = document.getElementById(printSectionId).innerHTML;
+        popupWin = window.open('', '_blank', 'top=0,left=0,height=100%,width=auto');
+        popupWin.document.open();
+        popupWin.document.write(`
+          <html>
+            <head>
+              <title>Print tab</title>
+              <style>
+              //........Customized style.......
+              </style>
+            </head>
+        <body onload="window.print();window.close()">${printContents}
+        <script type="text/javascript" src="inline.bundle.js"></script>
+        <script type="text/javascript" src="polyfills.bundle.js"></script>
+        <script type="text/javascript" src="styles.bundle.js"></script>
+        <script type="text/javascript" src="vendor.bundle.js"></script>
+        <script type="text/javascript" src="main.bundle.js"></script>
+        </body>
+        
+          </html>`
+        );
+        popupWin.document.close();
+      }
 
   ngOnInit() {
   }

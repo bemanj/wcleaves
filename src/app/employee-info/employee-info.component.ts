f97efad2abf9eec8f8256dcf548bbef57d45da0f
rjohn <rjohn-@users.noreply.github.com>
2017-10-17 14:38:30 +0800

    upload printing

--- src/app/employee-info/employee-info.component.ts
+++ src/app/employee-info/employee-info.component.ts
@@ -1,3 +1,4 @@
+import { Hero } from './../model/hero';
 import { Component, OnInit } from '@angular/core';
 import { AngularPrint } from 'angular-print';
 
@@ -7,7 +8,15 @@ import { AngularPrint } from 'angular-print';
   styleUrls: ['./employee-info.component.css']
 })
 export class EmployeeInfoComponent implements OnInit {
+  title = 'Midas';
+  heroes = [
+    new Hero(1, 2, 'Boxes', 'Goldcreek 27.68 @ 61', 1383.48, 2766.96),
+    new Hero(1, 1, 'KG', 'PHOS - 2AC 25KS', 400, 400),
+    new Hero(1, 1, 'Bag', 'PHOS -2AC', 16, 16),
+    new Hero(1, 500, 'Cut', '20 BXS PORK C.FAT ROSDERRA ', 58, 29000)
+  ];
 
+  myHero = this.heroes[0];
   constructor() { }
 
       print(printSectionId): void {
@@ -16,18 +25,26 @@ export class EmployeeInfoComponent implements OnInit {
         popupWin = window.open('', '_blank', 'top=0,left=0,height=100%,width=auto');
         popupWin.document.open();
         popupWin.document.write(`
-          <html>
-            <head>
-              <title>Print tab</title>
-              <style>
-              //........Customized style.......
-              </style>
-            </head>
-        <body onload="window.print();window.close()">${printContents}
-       
-        </body>
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="utf-8" />
+            <title> Receipt</title>
+            <style>
+                .val td {
+                    border: 1px solid black;
+                    border-collapse: collapse;
+                }
+            </style>
         
-          </html>`
+        
+        </head>
+        
+        <body class="text-left" onload="window.print();window.close()">${printContents}
+        </body>
+        </html>
+        
+          `
         );
         popupWin.document.close();
       }
---
+++ src/app/model/hero.ts
@@ -0,0 +1,9 @@
+export class Hero {
+    constructor(
+      public id: number,
+      public quantity: number,
+      public unit: string,
+      public articles: string,
+      public unitprice: number,
+      public amount: number,) { }
+  }
